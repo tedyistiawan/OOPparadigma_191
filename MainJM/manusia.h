c3de3cd181da37343865c9f59e4f5c7ad483b4d7
tedyistiawan <tedypnd07@gmail.com>
2023-05-22 18:57:33 +0700

    1

---
+++ MainJM/manusia.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#ifndef MANUSIA_H
+#define MANUSIA_H
+#include "Jantung.h"
+using namespace std;
+
+class manusia {
+public:
+    string name;
+    jantung varJantung;
+
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
+};
+#endif
+
